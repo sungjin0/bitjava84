fbd62002a52720201dc5ef077f743d9b8129d7b8
sungjin <sungjin@DESKTOP-169V1NC>
2016-07-28 15:41:47 +0900

    2016-07-26 commit

--- src/edu/bit/java/sungjin/QuickSortTest.java
+++ src/edu/bit/java/sungjin/QuickSortTest.java
@@ -21,7 +21,6 @@ class QuickSort {
 				temp = arr[left];
 				arr[left] = arr[right];
 				arr[right] = temp;
-				
 				if (left == right) {
 					Sort(arr, pivot, (left - 1));
 					Sort(arr, (left + 1), len);
@@ -36,10 +35,9 @@ class QuickSort {
 }
 
 public class QuickSortTest {
-	
 	public static void main(String[] args) {
 		int[] arr = new int[] { 41, 11, 5, 10, 160, 120, 150, 120, 110 };
 		int[] sort = QuickSort.Sort(arr, 0, arr.length - 1);
-//		System.out.println(Arrays.toString(sort));
+		// System.out.println(Arrays.toString(sort));
 	}
 }
